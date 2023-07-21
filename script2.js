22d75f66e782dfdfb18c5371ae54997a43599cef
Sayantan <maitysayantan111@gmail.com>
2023-07-21 19:32:15 +0530

    Coding School

---
+++ script2.js
@@ -0,0 +1,10 @@
+ //word vs keyword
+ //var const let variables & constants
+
+ var dulha = "Lab";
+ var dulhan = "Laby";
+
+ //hoisting: variables and functions are hoisted which means their declaration is moved on the top of code
+
+
+

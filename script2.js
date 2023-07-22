cf6d5b3977c5d17c1ae6a430cc161e6b320505f9
Sayantan <maitysayantan111@gmail.com>
2023-07-22 21:24:22 +0530

    slow

--- script2.js
+++ script2.js
@@ -1,3 +1,4 @@
+ 
  //word vs keyword
  //var const let variables & constants
 
@@ -5,6 +6,12 @@
  var dulhan = "Laby";
 
  //hoisting: variables and functions are hoisted which means their declaration is moved on the top of code
+
+ console.log(a);
+ var a=12;
+
+ //types in js
+ 
 
 
 

5c43e904b444cc3032d39d3440f513ba717b1532
Sayantan <maitysayantan111@gmail.com>
2023-07-24 09:54:48 +0530

    9:54

--- script2.js
+++ script2.js
@@ -2,16 +2,26 @@
  //word vs keyword
  //var const let variables & constants
 
- var dulha = "Lab";
- var dulhan = "Laby";
+//  var dulha = "Lab";
+//  var dulhan = "Laby";
 
  //hoisting: variables and functions are hoisted which means their declaration is moved on the top of code
 
- console.log(a);
- var a=12;
+//  console.log(a);
+//  var a=12;
 
  //types in js
-//  operator
+//  primitive & Reference
+
+//primitives
+//reference = [] {} ()
+//aisi koi bhi value jisko copy karne par real copy nahi hota, balki us main value ka reference pass hojata hai, use hum reference value kahate
+//hai, aur jisko copy karne par real copy ho jaaye wo value primitive type value hai
+var a;
+a = [12,23];
+var b = a;
+
+
 
 
 

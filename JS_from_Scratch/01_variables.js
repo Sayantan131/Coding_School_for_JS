026fe015e80fb73093b15a7c66879228beccd944
Sayantan <maitysayantan111@gmail.com>
2023-09-24 20:31:32 +0530

    running

---
+++ JS_from_Scratch/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="hitesh@google.com"
+var accountPassword = "12345"
+
+accountCity = "jaiour";
+// accountId=2;
+
+accountEmail="maitysayantan111@gmail.com"
+accountPassword="y32762736"
+accountCity="Kolkata";
+
+// console.log(accountId);//
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity]); 

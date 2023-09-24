fbac439071f3544efcd9fa0b61ea1bcdd83453ac
Sayantan <maitysayantan111@gmail.com>
2023-09-24 20:54:08 +0530

    running

---
+++ JS_from_Scratch/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//  alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3);
+
+
+let name= "sayantan";
+
+//premitive datatype
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined => value is not assign
+//symbol when we try find the uniqueness
+
+
+//object
+
+console.log(typeof "sayantan");

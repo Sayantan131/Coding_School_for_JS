8c6ec604ddda111f4041ff8df8a3e26ae51f6341
Sayantan <maitysayantan111@gmail.com>
2023-09-25 17:56:16 +0530

    running

---
+++ JS_from_Scratch/05_datatype_summery.js
@@ -0,0 +1,38 @@
+//primitive
+
+//7 types: String, Number, Bollean, null, undefined,  symbol,BigInt,
+
+
+const score  =100;
+
+const scoreValue=false;
+const outsideTemp = null;
+
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+const bigNumber = 21655641654946464666n; //bigInt datatype
+
+//Reference Type(Non primitive)
+
+//Array, Objects, Functions //I have to master object & event in browser
+
+
+const heros = ["shaktimna","nagarajj","doga"];
+let muObj= {
+    name :"sayantan",
+    age:22,
+}
+
+
+
+const myfunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+

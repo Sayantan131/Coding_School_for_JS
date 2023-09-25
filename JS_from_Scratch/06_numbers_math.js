3f40544bc5be257081cbdcdb0712cb1a1c2016dd
Sayantan <maitysayantan111@gmail.com>
2023-09-25 23:11:14 +0530

    running

---
+++ JS_from_Scratch/06_numbers_math.js
@@ -0,0 +1,16 @@
+const score = 400;
+
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+
+const otherName = 123.8966;
+
+console.log(otherName.toPrecision(1))
+
+const hundreds = 1000000;
+
+console.log(hundreds.toLocaleString('en-IN'));

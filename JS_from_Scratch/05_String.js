dc0e9620640621c3eeda30998a9cf25633a2cb8f
Sayantan <maitysayantan111@gmail.com>
2023-09-25 22:39:26 +0530

    running

---
+++ JS_from_Scratch/05_String.js
@@ -0,0 +1,9 @@
+const name = "Sayantan";
+const repoCount = 50;
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String (`sayantan`);
+
+console.log(gameName);
+

6f5a8e671eb3e496db81be8907162be5ccc67c15
Sayantan <maitysayantan111@gmail.com>
2023-09-25 09:37:09 +0530

    running

--- JS_from_Scratch/03_conversion.js
+++ JS_from_Scratch/03_conversion.js
@@ -6,3 +6,22 @@ console.log(typeof score);
 
 let valueInNumber = Number(score);;
 console.log(typeof valueInNumber);
+
+let isLoggedIn= "Sayantan";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=>true, 0=>false
+//""=>false
+//"hotesh"=>true
+
+  let someNumber = 33;
+
+
+  let stringNumber = String(someNumber);
+
+  console.log(stringNumber);
+  console.log(typeof stringNumber);
+
+  

fe940f9bae49dd0b0268a28e21a40addfd34f1ff
Sayantan <maitysayantan111@gmail.com>
2023-09-25 10:11:20 +0530

    running

--- JS_from_Scratch/03_conversion.js
+++ JS_from_Scratch/03_conversion.js
@@ -24,4 +24,18 @@ console.log(booleanIsLoggedIn);
   console.log(stringNumber);
   console.log(typeof stringNumber);
 
-  
+  //*************operations ***************/
+
+  console.log(2+3+"5")
+
+  let x = 3;
+let y = x++;
+
+console.log(x,y);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"

0bf1e29abf2514da6df0f7f89ac24e88a39b96d6
Sayantan <maitysayantan111@gmail.com>
2023-09-28 10:30:27 +0530

    running

--- JS_from_Scratch/06_numbers_math.js
+++ JS_from_Scratch/06_numbers_math.js
@@ -21,6 +21,35 @@ console.log(hundreds.toLocaleString('en-IN'));
 
 
  console.log(Math);
+
+ console.log(Math.abs(-4)); 
+
+ console.log(Math.round(4.5));
+
+ console.log(Math.ceil(4.4));
+
+ console.log(Math.floor(4.9));
+
+ console.log(Math.min(3,7,2,4,5));
+ console.log(Math.max(5,8,9,3,4,5));
+
+ console.log(Math.random());
+
+ console.log((Math.random()*10) + 1)
+
+ console.log(Math.floor(Math.random()*10)+1);  
+
+const max= 20;
+const min = 10;
+
+
+ console.log(Math.floor(Math.random() * (max-min+1)) + min)
+  
+
+  
+
+
+
  
 
 

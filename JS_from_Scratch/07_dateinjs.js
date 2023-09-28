74cc27a59930dd43e8608ae6658d9a555f0ca91c
Sayantan <maitysayantan111@gmail.com>
2023-09-28 21:10:12 +0530

    running

---
+++ JS_from_Scratch/07_dateinjs.js
@@ -0,0 +1,24 @@
+ //Dates
+
+let myDate = new Date();
+
+console.log(myDate.toString());
+
+console.log(myDate.toISOString());
+
+console.log(myDate.toJSON());
+
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23,5,3);
+
+let myCreatedDate = new Date("01-14-2023");
+
+// console.log(myCreatedDate.toDateString());
+
+// console.log(myCreatedDate.toLocaleString());
+
+
+

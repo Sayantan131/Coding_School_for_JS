7d1eaec20bc27d82074a58b3fb02f0257a6511e4
Sayantan <maitysayantan111@gmail.com>
2023-09-29 12:48:37 +0530

    running

---
+++ JS_from_Scratch/02_basics/01_array.js
@@ -0,0 +1,25 @@
+//array 
+
+const myArr = [0,1,2,34,4];
+
+    // console.log(myArr[3]);
+
+    // //Array Methods
+
+    // myArr.push(8);
+
+    // console.log(myArr);
+
+    // myArr.pop();
+
+
+    // myArr.unshift(9);
+    // myArr.shift();
+
+    
+
+
+    console.log(myArr);
+
+   
+

bc22e78b899a25f54856fe04e63435d5f12fb266
Sayantan <maitysayantan111@gmail.com>
2023-10-01 19:53:59 +0530

    running

--- JS_from_Scratch/02_basics/01_array.js
+++ JS_from_Scratch/02_basics/01_array.js
@@ -16,10 +16,28 @@ const myArr = [0,1,2,34,4];
     // myArr.unshift(9);
     // myArr.shift();
 
-    
+    // console.log(myArr.includes(9));
+    // console.log(myArr.indexOf(9));
 
 
-    console.log(myArr);
+    //  const newArr = myArr.join();
+
+    //  console.log(myArr);
+    //  console.log(newArr);
+
+
+    //slice,splice
+
+    const myn1 = myArr.slice(1,3);
+    console.log("A",myArr);
+
+
+    console.log(myn1);
+   console.log("B",myArr);
+
+    const myn2 = myArr.splice(1,3);
+    console.log("C",myArr);
+    console.log(myn2);
 
    
 

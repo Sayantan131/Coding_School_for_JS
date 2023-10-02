d5dc749c02bba7e62f0add4f1b8289473a4784a7
Sayantan <maitysayantan111@gmail.com>
2023-10-03 01:01:40 +0530

    running

--- JS_from_Scratch/02_basics/04_object.js
+++ JS_from_Scratch/02_basics/04_object.js
@@ -24,6 +24,37 @@ const obj1 = {1:"a",2:"b"};
 const obj2 = {3:"c",4:"d"};
 
 // const obj3 = {obj1,obj2};
-const obj3 = Object.assign(obj1,obj2);
 
-console.log(obj3);
+
+//const obj3 = {target,source}
+//in down line {} is target and obj1 & obj2 is source
+
+
+// const obj3 = Object.assign({},obj1,obj2);
+const obj3 = {...obj1,...obj2}; //spread operator
+
+// console.log(obj3);
+
+const users  = [
+    {
+        id:1,
+        email:"m@gamil.com"
+    },
+    {
+        id:1,
+        email:"m@gamil.com"
+    },
+    {
+        id:1,
+        email:"m@gamil.com"
+    }
+]
+
+users[1].email;
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('name'))

7e295c3060b13f8151e18c7742ff727137c55483
Sayantan <maitysayantan111@gmail.com>
2023-10-02 13:44:51 +0530

    running

---
+++ JS_from_Scratch/02_basics/03_objects.js
@@ -0,0 +1,45 @@
+//singleton
+
+//object literals
+
+const mySym = Symbol("key1");
+
+// Object.create
+const JsUser = {
+    name:"sayantan",
+    "full name":"Sayantan Maity",
+    [mySym]:"myKey1",
+    email:"maitysayantan111@gmail.com",
+    age:22,
+    location:"kolkata",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
+     
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser.age);
+// console.log(JsUser.location);
+// console.log(JsUser.isLoggedIn);
+// console.log(JsUser["full name"]);
+// console.log(typeof JsUser[mySym]);
+
+
+ JsUser.email = "maitysayntan67@gmail.com";
+//  console.log(JsUser.email);
+//  Object.freeze(JsUser);
+ JsUser.email = "maitysayantan@google.com";
+//  console.log(JsUser);
+
+
+  JsUser.greeting = function(){
+    console.log("Hello JS user");
+  }
+  
+  JsUser.greetingTwo = function(){
+    console.log(`Hello JS user,${this.name}`);
+  }
+
+  console.log(JsUser.greeting());
+  console.log(JsUser.greetingTwo());

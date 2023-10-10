ce371d0ed97c1b1a3310f286f9c0a2ac9466719b
Sayantan <maitysayantan111@gmail.com>
2023-10-10 11:00:53 +0530

    running

---
+++ just_a_example.js
@@ -0,0 +1,13 @@
+let val1 =10;
+let val2 = 5;
+
+function addNum(num1,num2){
+  total = num1+num2;
+  return total;
+}
+
+let result1 = addNum(val1,val2);
+let result2 = addNum(5,6);
+
+console.log(result1);
+console.log(result2);

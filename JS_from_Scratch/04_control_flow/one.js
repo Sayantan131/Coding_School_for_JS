e4fd59e4328b3334ab665657a7e1280705d22ba1
Sayantan <maitysayantan111@gmail.com>
2023-10-10 14:13:55 +0530

    if else

---
+++ JS_from_Scratch/04_control_flow/one.js
@@ -0,0 +1,57 @@
+//if statement
+
+
+if(false){
+
+}
+if(false){
+
+}
+
+const isUserloggedIn = true;
+const temperature = 41;
+
+// if(2 != 3){
+//     console.log("executed");
+// }
+
+// if(temperature===40){
+//     console.log("that is from if")
+// }else{
+//     console.log("that is from the else part")
+// }
+
+//<,>,<=, >=, ==, !=, ===, !==
+
+
+// *************Block Scope ************
+
+// const score = 200;
+// if(score>100){
+//     let power = "fly";
+//     console.log(`user power: ${power}`)
+// }
+// console.log(`user power: ${power}`);
+
+
+const balance = 4000;
+
+
+// aisa nahi karna hai , lagake
+// if(balance>500) console.log("test"), console.log("test2");
+
+
+// *****************8if else nested*************8
+
+if(balance<1000){
+    console.log("balance less than 1000")
+    
+}else if(balance<2000){
+
+    console.log("balance less than 2000")
+}else if(balance<3000){
+
+    console.log("balance less than 3000")
+}else{
+    console.log("balance less than 5000")
+}

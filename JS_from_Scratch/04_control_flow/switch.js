fb9aa8209fe997a02881bdc4641200928034e439
Sayantan <maitysayantan111@gmail.com>
2023-10-10 15:18:47 +0530

    switch

---
+++ JS_from_Scratch/04_control_flow/switch.js
@@ -0,0 +1,49 @@
+// switch (key){
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+
+// switch(month){
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;                //if this break is comment then after all the case will be executed except default
+//     case 4:
+//         console.log("April");
+//         break;
+//       default:
+//         console.log("default")
+//         break;  
+// }
+
+
+
+switch(month){
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;                //if this break is comment then after all the case will be executed except default
+    case "april":
+        console.log("April");
+        break;
+      default:
+        console.log("default")
+        break;  
+}
+

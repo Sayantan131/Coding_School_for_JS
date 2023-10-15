5fe575c62149657209975b78a603307420be87f1
Sayantan <maitysayantan111@gmail.com>
2023-10-15 11:36:06 +0530

    for of loop

---
+++ JS_from_Scratch/05_iterations/three.js
@@ -0,0 +1,32 @@
+//for of
+
+// ["","",""]
+
+// [{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "hello world!";
+
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`)
+}
+
+//Maps 
+
+const map = new Map()
+
+map.set('IN',"India")
+map.set('Fr',"France")
+map.set('USA',"United State of America")
+
+// console.log(map)
+
+for (const [key,value] of map) {
+    console.log(key, ':-' , value)
+}
+

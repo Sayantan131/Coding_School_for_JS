4c72baa78a40f43f89e87b148fa9e308222f471e
Sayantan <maitysayantan111@gmail.com>
2023-10-27 18:56:58 +0530

    foreach loop

--- JS_from_Scratch/05_iterations/five.js
+++ JS_from_Scratch/05_iterations/five.js
@@ -1,5 +1,46 @@
+//foreach loop
+
 const coding = ["js","ruby","C++","python","cpp"]
 
-coding.forEach( function (item){
-        console.log(item)
+// coding.forEach( function (item){
+//         console.log(item)
+// } )
+
+
+
+// coding.forEach( (item)=>{
+//     console.log(item)
+// } )
+
+
+// function printMe(item){
+//     console.log(item)
+// }
+
+// coding.forEach(printMe)
+
+
+// coding.forEach( (item, index, arr)=>{
+//     console.log(item, index, arr)
+// } )
+
+
+const myCoding = [
+    {
+        languageName : "JavaScript",
+        languageFileName: "js"
+    },
+    {
+        languageName : "Java",
+        languageFileName: "java"
+    },
+    {
+        languageName : "python",
+        languageFileName: "py"
+    }
+]
+
+
+myCoding.forEach( (item)=>{
+   console.log(item.languageName);
 } )

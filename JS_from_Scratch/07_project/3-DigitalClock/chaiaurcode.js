548ce6fef2419099e9a8d38a1f3db8b6e69bd3d6
Sayantan <maitysayantan111@gmail.com>
2023-11-05 21:52:49 +0530

    Digital Clock

---
+++ JS_from_Scratch/07_project/3-DigitalClock/chaiaurcode.js
@@ -0,0 +1,14 @@
+const clock = document.getElementById('clock')
+// console.log(clock);
+
+// let date = new Date();
+
+// console.log(date.toLocaleTimeString());
+
+
+setInterval(function(){
+    let date = new Date();
+
+    // console.log(date.toLocaleTimeString());  
+     clock.innerHTML = date.toLocaleTimeString();
+},1000)

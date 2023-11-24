52585b79051f57c3c6159ad2a6370eb132288a3a
Sayantan <maitysayantan111@gmail.com>
2023-11-24 23:56:21 +0530

    project 2 done

--- JS_from_Scratch/07_project/6-unlimitedColors/chaiaurcode.js
+++ JS_from_Scratch/07_project/6-unlimitedColors/chaiaurcode.js
@@ -7,4 +7,23 @@ const randomColor = function(){
     return color;
 }
 
-console.log(randomColor());
+let intervalId
+
+const startChangingColor = function(){
+    if(!intervalId){
+        intervalId = setInterval(changeColor,1000)
+    }
+    
+    function changeColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+
+}
+
+const stopChangingColor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+document.querySelector('#start').addEventListener('click',startChangingColor);
+document.querySelector('#stop').addEventListener('click',stopChangingColor);

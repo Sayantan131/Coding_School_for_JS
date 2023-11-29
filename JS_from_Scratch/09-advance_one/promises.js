758ad9f5a69169db7170edef6c7b275ee8b1a3c8
Sayantan <maitysayantan111@gmail.com>
2023-11-29 11:35:17 +0530

    promise is running

--- JS_from_Scratch/09-advance_one/promises.js
+++ JS_from_Scratch/09-advance_one/promises.js
@@ -60,4 +60,52 @@ new Promise(function(resolve,reject){
  })  
 
 
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({userName:"JavaScript",password:"1234"})
+        }else{
+            reject('ERROR: JS Went Wrong');
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive;
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+consumePromiseFive();
+
+// async function getAllUsers(){
+
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ",error)
+//     }
  
+// }
+
+// getAllUsers();
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log("E: ",error);
+})

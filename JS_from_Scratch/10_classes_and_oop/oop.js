554c0590c224b12edf445f4f4a5c2ee08f1a7b1f
Sayantan <maitysayantan111@gmail.com>
2023-12-02 21:28:13 +0530

    oop concept

--- JS_from_Scratch/10_classes_and_oop/oop.js
+++ JS_from_Scratch/10_classes_and_oop/oop.js
@@ -5,9 +5,13 @@ const user = {
     signedIn:true,
 
     getUserDetails: function(){
-        console.log("Got user details from database");
+      //  console.log("Got user details from database");
+       // console.log(`username: ${this.username}`);
+       console.log(this);
     }
 }
 
 console.log(user.username);
-console.log(user.getUserDetails())
+// console.log(user.getUserDetails())
+
+console.log(this);

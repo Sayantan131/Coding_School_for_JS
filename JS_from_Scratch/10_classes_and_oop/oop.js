06874166f7be35a373572a7fb392c5f618e874fe
Sayantan <maitysayantan111@gmail.com>
2023-12-03 18:27:59 +0530

    oop concept

--- JS_from_Scratch/10_classes_and_oop/oop.js
+++ JS_from_Scratch/10_classes_and_oop/oop.js
@@ -1,3 +1,5 @@
+
+//object literal
 const user = {
 
     username:"Sayantan",
@@ -11,7 +13,36 @@ const user = {
     }
 }
 
-console.log(user.username);
+// console.log(user.username);
 // console.log(user.getUserDetails())
 
-console.log(this);
+//console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+
+  this.username = username;
+  this.loginCount = loginCount;
+  this.isLoggedIn = isLoggedIn;
+
+  this.greetings = function(){
+    console.log(`Welcome ${this.username}`);
+  }
+
+  return this;
+}
+
+const userOne = new User('Sayantan',12,true);
+const userTwo =new User('Adrija',11,false);
+
+console.log(userOne);
+console.log(userTwo);
+
+/*
+after use of new keyword 
+i)a new object will create 
+ii)a constructor function will be called all arguments will be packed in it
+iii)for this key word all argument will store in the object
+iv)get the result
+*/
+

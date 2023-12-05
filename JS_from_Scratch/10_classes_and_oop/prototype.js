d4f7a3c0e7bded9bee5266cc515b8d9e34909822
Sayantan <maitysayantan111@gmail.com>
2023-12-05 21:48:09 +0530

    prototype

--- JS_from_Scratch/10_classes_and_oop/prototype.js
+++ JS_from_Scratch/10_classes_and_oop/prototype.js
@@ -5,6 +5,8 @@
 // console.log(myName.trueLength());
 
 
+
+
 let myHeros = ["thor","spiderman"]
 
 
@@ -33,4 +35,45 @@ Array.prototype.heySayantan = function(){
 myHeros.sayantan();
 myHeros.heySayantan();
 // heroPower.heySayantan();
+
+
+//inheritance
+
+const user = {
+    name:"Chai",
+    gmail:"chai@gmail.com"
+}
+
+const Teacher = {
+    makeVideo : true
+}
+
+const TeachingSupport={
+    isAvailable:false
+}
+
+const TASupport = {
+    makeAssignment :'JS assignment',
+    fullTime:true,
+    __proto__:TeachingSupport
+}
+
+Teacher.__proto__ = user;
+
+//modern syntax
+
+Object.setPrototypeOf(TeachingSupport,Teacher);
+
+
+let anotherUsername = "Chaiaurcode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"Sayantan".trueLength()
+"Adrija".trueLength()
+"icecream".trueLength()
 
